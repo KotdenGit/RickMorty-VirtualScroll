f88b0da9e17f748a75c51da3cb44fc0ef63b3533
Yury V <kotden09@gmail.com>
2025-05-07 14:39:06 +0300

    feat: Refactor app component and update assets
    - Converted AppComponent to a standalone component with inline template and styles.
    - Updated the favicon from 'favicon.ico' to 'favicon.png'.
    - Added new favicon image.
    - Introduced a new logo SVG asset for branding.

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -2,11 +2,35 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+  <style>
+    :host {
+    --content-padding: 10px;
+  }
+  header {
+    display: block;
+    height: 100px;
+    padding: var(--content-padding);
+    box-shadow: 0px 5px 25px var(--shadow-color);
+  }
+  .content {
+    padding: var(--content-padding);
+  }
+  </style>
+  <main>
+    <header class="brand-name">
+    <img class="brand-logo" src="assets/logo.svg"
+    alt="Logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  title = 'my app';
 }
