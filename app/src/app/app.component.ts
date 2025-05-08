7351aa0f254138220966f6d737e6c37a26d2364e
Yury V <kotden09@gmail.com>
2025-05-08 13:20:49 +0300

    feat: enhance app styling and layout structure

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -8,22 +8,36 @@ import { RouterOutlet } from '@angular/router';
   template: `
   <style>
     :host {
-    --content-padding: 10px;
-  }
-  header {
-    display: block;
-    height: 100px;
-    padding: var(--content-padding);
-    box-shadow: 0px 5px 25px var(--shadow-color);
-  }
-  .content {
-    padding: var(--content-padding);
-  }
+      --content-padding: 10px;
+    }
+    header {
+      position: relative;
+      display: block;
+      height: 100px;
+      overflow: hidden;
+      padding: var(--content-padding);
+      box-shadow: 0px 5px 25px var(--shadow-color);
+      background-color: var(--primary-color);
+      background: linear-gradient(90deg, var(--primary-color), var(--secondary-color));
+      z-index: -1;
+    }
+    header::before {
+      content: "";
+      position: absolute;
+      top: -250%; 
+      left: -40%; 
+      width: 150%;
+      height: 900%;
+      background: url("/assets/logo.svg") repeat;
+      background-size: 50px 50px;
+      transform: rotate(-30deg);
+    }
+    .content {
+      padding: var(--content-padding);
+    }
   </style>
   <main>
     <header class="brand-name">
-    <img class="brand-logo" src="assets/logo.svg"
-    alt="Logo" aria-hidden="true">
     </header>
     <section class="content">
       <router-outlet></router-outlet>
