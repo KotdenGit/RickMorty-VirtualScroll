3bc06714f861cd315b19211388a684a002bbe689
Yury V <kotden09@gmail.com>
2025-05-09 13:29:10 +0300

    feat: add CharacterService with method to fetch all characters

---
+++ app/src/app/character.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Individual } from './individual';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharacterService {
+  url = 'http://localhost:3000/locations';
+
+  constructor() { }
+
+  async getAllCharacters(): Promise<Individual[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? []; 
+  }
+}
--- app/src/app/character/character.component.ts
+++ app/src/app/character/character.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { IndividualComponent } from '../individual/individual.component';
 
 @Component({
   selector: 'app-character',
-  imports: [IndividualComponent],
+  imports: [CommonModule, IndividualComponent],
   template: `
     <section>
       <form>  
