5e08ab11405961a94b3b9200015d9cb748c2fab2
Yury V <kotden09@gmail.com>
2025-05-09 11:55:47 +0300

    feat: add IndividualComponent with basic template

--- app/src/app/character/character.component.ts
+++ app/src/app/character/character.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { IndividualComponent } from '../individual/individual.component';
 
 @Component({
   selector: 'app-character',
-  imports: [],
+  imports: [IndividualComponent],
   template: `
     <section>
       <form>  
         <input type="text" placeholder="Filter by name" #filter>
         <button class="primary" type="button" >Search</button>
       </form>
+    </section>
+    <section class="results">
+      <app-individual></app-individual>
     </section>
   `,
   styleUrl: './character.component.scss'
---
+++ app/src/app/individual/individual.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-individual',
+  imports: [],
+  template: `
+    <p>
+      individual works!
+    </p>
+  `,
+  styleUrl: './individual.component.scss'
+})
+export class IndividualComponent {
+
+}
